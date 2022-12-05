b8cd4a811786c7c788e5b1a5444b292ed7352a24
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2022-12-05 23:26:59 +1100

    Link the project to mongo db the app able to insert and fetch data from the pet database

---
+++ public/js/script.js
@@ -0,0 +1,87 @@
+/*
+Create a addCards Function will take items as an inputs
+will go through the items for each item will sign the 
+item objects such as image, title, link, and description into
+a variable itemToAppend with html code.
+
+*/
+const addCards = (items) => {
+  items.forEach((item) => {
+    let itemToAppend =
+      '<div class="col s4 center-align">' +
+      '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="./images/' +
+      item.image +
+      '">' +
+      '</div><div class="card-content">' +
+      '<span class="card-title activator grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+      item.link +
+      '</a></p></div>' +
+      '<div class="card-reveal">' +
+      '<span class="card-title grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">close</i></span>' +
+      '<p class="card-text">' +
+      item.description +
+      '</p>' +
+      '</div></div></div>'
+    $('#card-section').append(itemToAppend)
+  })
+}
+/*
+submitForm
+is a function that collect the inputs form popup window
+model firstName, lastName, password, and email.
+signing the value into the object fromData
+and show the data in the console log.
+
+*/
+const submitForm = () => {
+  let formData = {}
+  formData.title = $('#title').val()
+  formData.image = $('#image').val()
+  formData.link = $('#link').val()
+  formData.description = $('#description').val()
+
+  console.log('Form Data Submitted: ', formData)
+  addProjectToApp(formData)
+}
+//ajax
+const addProjectToApp = (project) => {
+  $.ajax({
+    url: '/api/projects',
+    data: project,
+    type: 'POST',
+    success: (result) => {
+      location.reload() // it automatically  reload the page
+    },
+  })
+}
+
+const getProjects = () => {
+  $.get('/api/projects', (response) => {
+    if (response.statusCode == 200) {
+      addCards(response.data)
+    }
+  })
+}
+
+/* after the page load will sign and call the function 
+which is 
+materialbox initializing the material box with jQuery
+add listener to button submit event click
+submitForm function will call when clicking on the submit button 
+addCards will add cards to index html page by a append them to the end of
+card-section div.
+model() function will initialize  modal with jQuery 
+*/
+
+$(document).ready(function () {
+  $('.materialboxed').materialbox()
+  $('#formSubmit').click(() => {
+    submitForm()
+  })
+  getProjects()
+  $('.modal').modal()
+})
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express')
+var app = express()
+var cors = require('cors')
+let projectCollection
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+//mongoDB Connection
+const MongoClient = require('mongodb').MongoClient
+
+//add the database connections...
+const uri =
+  'mongodb+srv://Aalkinani:Aalkinani@pets.48befsl.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true })
+// Create collection
+const createCollection = (collectionName) => {
+  client.connect((err, db) => {
+    projectCollection = client.db().collection(collectionName)
+    if (!err) {
+      console.log('MongoDB connected')
+    } else {
+      console.log('DB Error: ', err)
+      process.exit(1)
+    }
+  })
+}
+//Insert project
+const insertProjects = (project, callback) => {
+  projectCollection.insert(project, callback)
+}
+// post api.....
+app.post('/api/projects', (req, res) => {
+  console.log('new Project added', req.body)
+  var newProject = req.body
+  insertProjects(newProject, (err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err })
+    } else {
+      res.json({
+        statusCode: 200,
+        message: 'Project Successfully added',
+        data: result,
+      })
+    }
+  })
+})
+//get project...
+const getProjects = (callback) => {
+  projectCollection.find({}).toArray(callback)
+}
+
+app.get('/api/projects', (req, res) => {
+  getProjects((err, result) => {
+    if (err) {
+      res.json({ statusCode: 400, message: err })
+    } else {
+      res.json({ statusCode: 200, message: 'Success', data: result })
+    }
+  })
+})
+
+var port = process.env.port || 3000
+
+app.listen(port, () => {
+  console.log('App listening to: ' + port)
+  createCollection('Pets')
+})
